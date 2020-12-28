a5ce0e95fd1a689657844c9a2a852fe8f20ab815
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2020-12-28 13:23:07 +0800

    fix: jlab 2 capability

--- src/index.ts
+++ src/index.ts
@@ -5,25 +5,17 @@ import {
 
 import { IThemeManager } from '@jupyterlab/apputils';
 
-import { ITranslator } from '@jupyterlab/translation';
-
 /**
  * A plugin for jupyterlab-theme-solarized-dark
  */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-theme-solarized-dark:plugin',
-  requires: [IThemeManager, ITranslator],
-  activate: function(
-    app: JupyterFrontEnd,
-    manager: IThemeManager,
-    translator: ITranslator
-  ) {
+  requires: [IThemeManager],
+  activate: function(app: JupyterFrontEnd, manager: IThemeManager) {
     const style = 'jupyterlab-theme-solarized-dark/index.css';
-    const trans = translator.load('jupyterlab');
 
     manager.register({
       name: 'JupyterLab Solarized Dark',
-      displayName: trans.__('JupyterLab Light'),
       isLight: false,
       themeScrollbars: true,
       load: () => manager.loadCSS(style),
